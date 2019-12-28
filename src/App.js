8f753bdd5bdafca00ce3d6992e28526777a8f7b2
ghazal1996 <fatememsk7@gmail.com>
2019-12-28 10:33:09 +0330

    add hello

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default class App extends Component {
                 <div className="App">
                     <header className="App-header">
                     <h1 className="App-title">Welcome to my web store</h1>
+                    <h2>hellooo</h2>
                     </header>
                 </div>
             </>
